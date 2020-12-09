9b8c43da6b1dcce0d8c865d116235699e844397d
Karan Thakkar <karant0007@gmail.com>
2020-12-09 15:05:00 +0530

    Weather Forecast Updates

--- Frontend Code/Controllers/HomeController.cs
+++ Frontend Code/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using Lawn_Mow_App.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -70,7 +74,7 @@ namespace Lawn_Mow_App.Controllers
                         model.Discount = "15%";
                         model.TotalAmount = input.SqMeter;
                         model.DiscountAmount = disAmnt;
-                        model.NetAmount= netAmnt;
+                        model.NetAmount = netAmnt;
                     }
                     TempData["Success"] = "Square Meter Calculation Done";
                     return View(model);
@@ -85,6 +89,40 @@ namespace Lawn_Mow_App.Controllers
                 TempData["Error"] = "Something Went Wrong";
             }
             return View();
+        }
+
+        public async Task<ActionResult> WeatherForecast()
+        {
+            var model = new WeatherForecast();
+            try
+            {
+                var APIkey = System.Configuration.ConfigurationManager.AppSettings["weatherAPIKey"];
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/");
+                    //HTTP GET
+                    var responseTask = client.GetAsync("onecall?lat=40.751&lon=-73.997&units=metric&appid=" + APIkey);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.StatusCode == HttpStatusCode.OK)
+                    {
+                        var responseString = await result.Content.ReadAsStringAsync();
+                        model = JsonConvert.DeserializeObject<WeatherForecast>(responseString);
+                        TempData["Success"] = "Next 7 Days Weather Report";
+                    }
+                    else
+                    {
+                        TempData["Error"] = "Failed To Getting Weather Data";
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = "Something Went Wrong";
+                model = null;
+            }
+            return View(model);
         }
     }
 }
